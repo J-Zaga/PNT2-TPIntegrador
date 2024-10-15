75d2446ae87408f90a45a2814e500571e254a5ba
J-Zaga <juan.zagarese@gmail.com>
2024-10-15 00:54:23 -0300

    Login de usuario empezado

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginUsuario from '../components/Log-in-usuario.vue'
+import LoginAdministrador from '../components/Log-in-prestador.vue'
+import LoginPrestador from '../components/Log-in-administrador.vue'
+
+const routes = [
+    {
+      path: '/usuario',
+      name: 'Login Usuario',
+      component: LoginUsuario,
+    },
+    {
+        path: '/administrador',
+        name: 'Login Admninistrador',
+        component: LoginAdministrador
+    },
+    {
+        path: '/prestador',
+        name: 'Login Prestador',
+        component: LoginPrestador
+    }
+  ]
+ 
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+  export default router;
