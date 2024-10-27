a48a5552c0d0bf565c6b5e169d79c0fd86d5053d
sharlenneazuaje <sharlenne8@gmail.com>
2024-10-27 18:46:50 -0300

    Buscador listo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,7 +19,7 @@ const filteredServices = computed(() => {
 
 <template>
     <div class="home-container">
-    <Buscador v-model:searchQuery="busqueda"/>
+    <Buscador v-if="usuarioActual" v-model:searchQuery="busqueda"/>
   
     <section class="services">
         <div v-if="filteredServices.length === 0" class="no-results">
@@ -35,7 +35,6 @@ const filteredServices = computed(() => {
 </template>
 
 <style scoped>
-
 .home-container {
   margin-top: 40px;
   margin-bottom: 40px;
@@ -46,32 +45,6 @@ const filteredServices = computed(() => {
   align-items: center; 
   padding: 20px;
   gap: 20px; 
-}
-
-.filters {
-  width: 100%;
-  max-width: 600px;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-}
-
-.filters input[type="text"] {
-  color: black;
-  width: 100%;
-  padding: 15px;
-  border-radius: 8px;
-  border: 1px solid #ccc;
-  font-size: 1.2rem;
-  background-color: #fff;
-  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.05);
-  transition: border-color 0.2s ease-in-out;
-}
-
-.filters input[type="text"]:focus {
-  border-color: #007bff;
-  outline: none;
 }
 
 .services {
